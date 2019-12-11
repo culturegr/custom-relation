33c40576c1b916fbfb085c9f19cca9e9b2272960
Iraklis Georgas <igeorgas@culture.gr>
2019-12-11 15:20:02 +0200

    Upgrade TestCase to work with Codeship CI

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -18,4 +18,20 @@ abstract class TestCase extends Orchestra
     {
         return ['CultureGr\CustomRelation\CustomRelationServiceProvider'];
     }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
 }
